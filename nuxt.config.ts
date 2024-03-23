9bce09d9c9925aa9642fc41eb2b6e165cae66b8b
naoki <naokipgt@gmail.com>
2024-03-23 13:16:03 +0900

    pwa対応仮

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,38 @@
+import path from 'path'
+import fs from 'fs'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   build: {
     transpile: ['vuetify']
+  },
+  app: {
+    head: {
+      meta: [
+        { name: 'theme-color', content: '#326CB3' }
+      ],
+      link: [
+        { rel: 'manifest', href: 'manifest.webmanifest' },
+        { rel: 'icon', href: '/favicon.ico', sizes: '48x48' },
+        { rel: 'icon', href: '/icon.svg', sizes: 'any', type: 'image/svg+xml' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon-180x180.png' }
+      ]
+    }
   },
   plugins: [
     '~/plugins/vuetify.ts'
   ],
   modules: [
+    '@vite-pwa/nuxt',
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }))
       })
     }
-    // ...
   ],
   vite: {
     vue: {
@@ -28,5 +44,52 @@ export default defineNuxtConfig({
   devServer: {
     port: 3000,
     host: '0.0.0.0'
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico'],
+    client: {
+      installPrompt: true
+    },
+    manifest: {
+      name: 'nuxt-apps',
+      description: 'description',
+      theme_color: '#326CB3',
+      lang: 'ja',
+      short_name: 'apps',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#FFFFFF',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: null
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    }
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
     hoge
-    <v-btn>
+    <v-btn class="btn" @click="click">
       hoge
     </v-btn>
   </div>
 </template>
+
+<script setup lang="ts">
+const click = () => {
+  console.log('click')
+}
+</script>
+
+<style scoped lang="scss">
+.btn {
+  color: red;
+  background-color: blue;
+
+  &:hover {
+    color: blue;
+    background-color: red;
+  }
+}
+</style>
