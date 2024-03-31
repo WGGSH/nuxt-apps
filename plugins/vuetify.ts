a7f70258444380a6910300dc59eafdc785f4bbcc
naoki <naokipgt@gmail.com>
2024-03-31 23:03:40 +0900

    issue-16 旧ディレクトリ削除

--- plugins/vuetify.ts
+++
@@ -1,8 +0,0 @@
-// import this after install `@mdi/font` package
-import 'vuetify/styles';
-
-import vuetify from '~/utils/vuetify';
-
-export default defineNuxtPlugin((app) => {
-  app.vueApp.use(vuetify);
-});
--- types/mine-sweeper.ts
+++
@@ -1,7 +0,0 @@
-type Cell = {
-  isMine: boolean;
-  status: 'hidden' | 'flagged' | 'revealed';
-  mineCount: number;
-};
-
-export type { Cell };
