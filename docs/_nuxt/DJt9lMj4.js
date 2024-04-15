33ce864da218e645654b1dff0487e205720818dd
naoki <naokipgt@gmail.com>
2024-04-16 00:48:19 +0900

    release

---
+++ docs/_nuxt/DJt9lMj4.js
@@ -0,0 +1,1 @@
+import{Z as n,$ as o,a0 as r,a1 as l,aM as i,aR as c,z as u}from"./C-I2TLMd.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{C as V};
