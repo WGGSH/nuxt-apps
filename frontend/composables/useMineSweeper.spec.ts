35aad5ca030911c91b769b91e3193b7b9545ab29
naoki <naokipgt@gmail.com>
2024-04-15 23:57:05 +0900

    issue-15: マインスイーパのテスト途中まで

--- frontend/composables/useMineSweeper.spec.ts
+++ frontend/composables/useMineSweeper.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, context } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { setActivePinia, createPinia } from 'pinia';
 import { useMineSweeper } from './useMineSweeper';
 
@@ -18,13 +18,15 @@ describe('useMineSweeper', () => {
     });
 
     it('field が初期化される', () => {
-      expect(mineSweeper.field).toEqual(Array.from({ length: CONSTANTS.fieldY }, () =>
-        Array.from({ length: CONSTANTS.fieldX }, () => ({
-          isMine: false,
-          status: 'hidden',
-          mineCount: 0,
-        })),
-      ));
+      expect(mineSweeper.field).toEqual(
+        Array.from({ length: CONSTANTS.fieldY }, () =>
+          Array.from({ length: CONSTANTS.fieldX }, () => ({
+            isMine: false,
+            status: 'hidden',
+            mineCount: 0,
+          })),
+        ),
+      );
     });
 
     it('isGameClear が false', () => {
@@ -40,14 +42,18 @@ describe('useMineSweeper', () => {
     describe('旗が立っていない状態で実行すると', () => {
       beforeEach(() => {
         mineSweeper.initialize();
-        mineSweeper.field.forEach(row => row.forEach(cell => (cell.status = 'hidden')));
+        mineSweeper.field.forEach(row =>
+          row.forEach(cell => (cell.status = 'hidden')),
+        );
 
         mineSweeper.mineCount = CONSTANTS.mineCount;
         mineSweeper.start(3, 3);
       });
 
       it('爆弾の数が正しい', () => {
-        const mineCount = mineSweeper.field.flat().filter(cell => cell.isMine).length;
+        const mineCount = mineSweeper.field
+          .flat()
+          .filter(cell => cell.isMine).length;
         expect(mineCount).toBe(CONSTANTS.mineCount);
       });
 
@@ -62,7 +68,9 @@ describe('useMineSweeper', () => {
     describe('周囲に爆弾がない場合', () => {
       beforeEach(() => {
         mineSweeper.initialize();
-        mineSweeper.field.forEach(row => row.forEach(cell => (cell.isMine = false)));
+        mineSweeper.field.forEach(row =>
+          row.forEach(cell => (cell.isMine = false)),
+        );
       });
 
       it('0 が返る', () => {
@@ -74,7 +82,9 @@ describe('useMineSweeper', () => {
     describe('周囲に爆弾が1つある場合', () => {
       beforeEach(() => {
         mineSweeper.initialize();
-        mineSweeper.field.forEach(row => row.forEach(cell => (cell.isMine = false)));
+        mineSweeper.field.forEach(row =>
+          row.forEach(cell => (cell.isMine = false)),
+        );
         mineSweeper.field[3][3].isMine = true;
       });
 
@@ -87,7 +97,9 @@ describe('useMineSweeper', () => {
     describe('周囲に爆弾が8つある場合', () => {
       beforeEach(() => {
         mineSweeper.initialize();
-        mineSweeper.field.forEach(row => row.forEach(cell => (cell.isMine = true)));
+        mineSweeper.field.forEach(row =>
+          row.forEach(cell => (cell.isMine = true)),
+        );
         mineSweeper.field[4][4].isMine = false;
       });
 
@@ -99,17 +111,89 @@ describe('useMineSweeper', () => {
   });
 
   describe('holdCell', () => {
+    describe('ゲームオーバー中の場合', () => {
+      beforeEach(() => {
+        mineSweeper.isGameOver = true;
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('何もしない', () => {
+        expect(mineSweeper.field[0][0].status).toBe('hidden');
+      });
+    });
+
+    describe('ゲームクリア時の場合', () => {
+      beforeEach(() => {
+        // mineSweeper.isGameClear = true;
+        mineSweeper.field.forEach(row =>
+          row.forEach(cell => (cell.status = 'revealed')),
+        );
+
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('何もしない', () => {
+        expect(mineSweeper.field[0][0].status).toBe('revealed');
+      });
+    });
+
+    describe('ゲーム開始後初回クリックの場合', () => {
+      beforeEach(() => {
+        mineSweeper.initialize();
+        mineSweeper.isFirstClick = true;
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('何もしない', () => {
+        expect(mineSweeper.field[0][0].status).toBe('hidden');
+      });
+    });
+
+    describe('すでに開いているセルをクリックした場合', () => {
+      beforeEach(() => {
+        mineSweeper.initialize();
+        mineSweeper.field[0][0].status = 'revealed';
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('何もしない', () => {
+        expect(mineSweeper.field[0][0].status).toBe('revealed');
+      });
+    });
+
+    describe('旗が立っているセルをクリックした場合', () => {
+      beforeEach(() => {
+        mineSweeper.initialize();
+        mineSweeper.start(3, 3);
+        mineSweeper.field[0][0].status = 'flagged';
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('旗を下ろす', () => {
+        expect(mineSweeper.field[0][0].status).toBe('hidden');
+      });
+    });
+
+    describe('旗が立っていないセルをクリックした場合', () => {
+      beforeEach(() => {
+        mineSweeper.initialize();
+        mineSweeper.start(3, 3);
+        mineSweeper.holdCell(0, 0);
+      });
+
+      it('セルを開く', () => {
+        expect(mineSweeper.field[0][0].status).toBe('flagged');
+      });
+    });
   });
 
-  describe('clickCell', () => {
-  });
+  // describe('clickCell', () => {
+  //
+  // });
 
-  describe('revealNeighbors', () => {
-  });
-
-  describe('setLevel', () => {
-  });
-
-  describe('getNeighborFlags', () => {
-  });
+  // describe('revealNeighbors', () => { });
+  //
+  // describe('setLevel', () => { });
+  //
+  // describe('getNeighborFlags', () => { });
 });
